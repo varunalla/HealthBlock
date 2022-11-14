d170b0748b2474c5caa2b163f7b3c36c9f976559
aishwaryaravi19 <aishwaryaravi19@gmail.com>
2022-11-13 20:59:49 -0800

    Added contract for doctor transaction

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,6 +1,8 @@
 var HealthBlock = artifacts.require("HealthBlock");
+var DoctorHealthBlock = artifacts.require("DoctorHealthBlock");
 
 module.exports = async function (deployer) {
     // deployment steps
     await deployer.deploy(HealthBlock);
+    await deployer.deploy(DoctorHealthBlock);
 };
