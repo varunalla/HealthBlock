96a1c23112d33de99fd295ac6b1ec899315a69dd
Varun Alla <varun.alla@gmail.com>
2023-04-04 00:38:06 -0700

    RBAC implementation with front end validations. added prettier for better formatting

--- client/src/components/Patient/PatientRegister.tsx
+++ client/src/components/Patient/PatientRegister.tsx
@@ -1,77 +1,132 @@
-
-import React, { FunctionComponent, useContext, useEffect, useState } from "react";
+import React, { FunctionComponent, useContext, useEffect, useState } from 'react';
 import { HealthContext } from '../../providers/HealthProvider';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-
-const PatientRegister: FunctionComponent<{}> = ({ }) => {
-    const { currentAccount, healthBlockContract, registerHealthBlockContract, fetchPatientContract } = useContext(HealthContext);
-    const [name, setName] = useState<string>("varun");
-    const [age, setAge] = useState<number>(32);
-    const [email, setEmail] = useState<string>("abc@abc.com");
-    const registerPatient = async () => {
-        try {
-            await registerHealthBlockContract?.(name, age, email);
-        }
-        catch (err) {
-            console.log('Register Error',err);
-        }
+const PatientRegister: FunctionComponent<{}> = ({}) => {
+  const {
+    currentAccount,
+    healthBlockContract,
+    registerHealthBlockContract,
+    fetchPatientContract,
+    fetchPatientInfoContract,
+  } = useContext(HealthContext);
+  const [name, setName] = useState<string>('varun');
+  const [age, setAge] = useState<number>(32);
+  const [email, setEmail] = useState<string>('abc@abc.com');
+  const registerPatient = async () => {
+    try {
+      //read patient info
+      const user = await fetchPatientContract?.();
+      if (!user?.email) {
+        toast('Patient Registration Initiated!');
+        await registerHealthBlockContract?.(name, age, email);
+        //feed back to user for registration
+        toast('Patient Created');
+      } else {
+        toast.error('User already present', {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: 'colored',
+        });
+      }
+    } catch (err) {
+      toast.error('Error Registering Patient, please try after sometime!');
+      console.log('Register Error', err);
     }
-    const fetchPatient = async () => {
-        try {
-            await fetchPatientContract?.();
-        }
-        catch (err) {
-            console.log('Fetch Patient Error',err);
-        }
+  };
+  const fetchPatient = async () => {
+    try {
+      await fetchPatientContract?.();
+    } catch (err) {
+      console.log('Fetch Patient Error', err);
     }
-    useEffect(() => {
-        healthBlockContract?.();
-    }, []);
-    return (
+  };
+  useEffect(() => {
+    healthBlockContract?.();
+  }, []);
+  return (
+    <>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='colored'
+      />
+      <form className='w-full max-w-sm' onSubmit={(e) => e.preventDefault()}>
+        <div className='md:flex md:items-center mb-6'>
+          <div className='md:w-1/3'>
+            <label className='block text-white font-bold md:text-right mb-1 md:mb-0 pr-4'>
+              Full Name
+            </label>
+          </div>
+          <div className='md:w-2/3'>
+            <input
+              className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+              id='inline-full-name'
+              type='text'
+              value={name}
+              onChange={(e) => setName(e.currentTarget.value)}
+            />
+          </div>
+        </div>
+        <div className='md:flex md:items-center mb-6'>
+          <div className='md:w-1/3'>
+            <label className='block text-white  font-bold md:text-right mb-1 md:mb-0 pr-4'>
+              Age
+            </label>
+          </div>
+          <div className='md:w-2/3'>
+            <input
+              className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+              id='inline-full-name'
+              type='number'
+              value={age}
+              onChange={(e) =>
+                e.currentTarget.value === '' ? setAge(0) : setAge(parseInt(e.currentTarget.value))
+              }
+            />
+          </div>
+        </div>
+        <div className='md:flex md:items-center mb-6'>
+          <div className='md:w-1/3'>
+            <label className='block text-white  font-bold md:text-right mb-1 md:mb-0 pr-4'>
+              Email
+            </label>
+          </div>
+          <div className='md:w-2/3'>
+            <input
+              className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+              id='inline-full-name'
+              type='email'
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+          </div>
+        </div>
+        <div className='md:flex md:items-center mb-6'>
+          <button
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded'
+            onClick={() => registerPatient()}
+          >
+            Register Patient
+          </button>
+          {/*<button onClick={() => fetchPatient()}>Fetch Patient</button> */}
+        </div>
+      </form>
+    </>
+  );
+};
 
-
-
-
-        <form className="w-full max-w-sm" onSubmit={e => e.preventDefault()}>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-white font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Full Name
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="text" value={name}
-                        onChange={(e) => setName(e.currentTarget.value)} />
-                </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-white  font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Age
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="number" value={age}
-                        onChange={(e) => e.currentTarget.value === '' ? setAge(0) : setAge(parseInt(e.currentTarget.value))} />
-                </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-                <div className="md:w-1/3">
-                    <label className="block text-white  font-bold md:text-right mb-1 md:mb-0 pr-4">
-                        Email
-                    </label>
-                </div>
-                <div className="md:w-2/3">
-                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" id="inline-full-name" type="email" value={email}
-                        onChange={(e) => setEmail(e.currentTarget.value)} />
-                </div>
-            </div>
-            <div className="md:flex md:items-center mb-6">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={() => registerPatient()}>Register Patient</button>
-                {/*<button onClick={() => fetchPatient()}>Fetch Patient</button> */}
-            </div>
-        </form>
-    )
-}
-
-export default PatientRegister
+export default PatientRegister;
--- client/src/providers/HealthProvider.tsx
+++ client/src/providers/HealthProvider.tsx
@@ -1,119 +1,145 @@
-import { ethers } from "ethers";
-import React, { useState } from "react";
+import { ethers } from 'ethers';
+import React, { useState } from 'react';
 
-import { healthBlockABI, healthBlockAddress } from "../config/constants";
+import { healthBlockAddress } from '../config/constants';
 import Web3Modal from 'web3modal';
 import { HealthBlock__factory } from '../contracts/factories/HealthBlock__factory';
 import { HealthBlock } from '../contracts/HealthBlock';
 
+export type Patient = {
+  name: string;
+  age: number;
+  email: string;
+};
+
 interface HealthAppContextInterface {
-    checkIfWalletIsConnected?: () => Promise<void>,
-    connectWallet?: () => Promise<void>,
-    healthBlockContract?: () => Promise<void>,
-    registerHealthBlockContract?: (name: string, age: number, email: string) => Promise<void>,
-    registerDoctorHealthBlockContract?: (name: string, age: number, email: string, specialization: string) => Promise<void>,
-    fetchPatientContract?: () => Promise<void>,
-    fetchPatientInfoContract?: (address: string) => Promise<void>,
-    currentAccount?: string
+  checkIfWalletIsConnected?: () => Promise<void>;
+  connectWallet?: () => Promise<void>;
+  healthBlockContract?: () => Promise<void>;
+  registerHealthBlockContract?: (name: string, age: number, email: string) => Promise<void>;
+  registerDoctorHealthBlockContract?: (
+    name: string,
+    age: number,
+    email: string,
+    specialization: string,
+  ) => Promise<void>;
+  fetchPatientContract?: () => Promise<Patient | undefined>;
+  fetchPatientInfoContract?: (address: string) => Promise<void>;
+  currentAccount?: string;
 }
 
-const fetchContract = (signerOrProvider: ethers.Signer | ethers.providers.Provider) => HealthBlock__factory.connect(healthBlockAddress, signerOrProvider);
-
+const fetchContract = (signerOrProvider: ethers.Signer | ethers.providers.Provider) =>
+  HealthBlock__factory.connect(healthBlockAddress, signerOrProvider);
 
 export const HealthContext = React.createContext<HealthAppContextInterface>({});
 
 interface Props {
-    children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 export const HealthProvider: React.FC<Props> = ({ children, ...props }) => {
-    const [currentAccount, setCurrentAccount] = useState('');
-    const [error, setError] = useState('');
+  const [currentAccount, setCurrentAccount] = useState('');
+  const [error, setError] = useState('');
 
-    //fetch metamask accounts
-    const checkIfWalletIsConnected = async () => {
-        if (!window.ethereum) return setError("Please Install Metamask");
-        const account = await window.ethereum.request?.({ method: "eth_accounts" });
-        if (account?.length) {
-            setCurrentAccount(account[0]);
-        }
-        else {
-            return setError("Please Install Metamask& connect, reload");
-        }
+  //fetch metamask accounts
+  const checkIfWalletIsConnected = async () => {
+    if (!window.ethereum) return setError('Please Install Metamask');
+    const account = await window.ethereum.request?.({ method: 'eth_accounts' });
+    if (account?.length) {
+      setCurrentAccount(account[0]);
+    } else {
+      return setError('Please Install Metamask& connect, reload');
     }
-    //connect metamask wallet
-    const connectWallet = async () => {
-        if (!window.ethereum) return setError("Please Install Metamask");
-        const account = await window.ethereum.request?.({ method: "eth_requestAccounts" });
-        setCurrentAccount(account[0]);
+  };
+  //connect metamask wallet
+  const connectWallet = async () => {
+    if (!window.ethereum) return setError('Please Install Metamask');
+    const account = await window.ethereum.request?.({ method: 'eth_requestAccounts' });
+    setCurrentAccount(account[0]);
+  };
+  //connect contract
+  const healthBlockContract = async () => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract = fetchContract(signer);
+    } catch (err) {
+      setError(`Error Loading Health Contract ${err}`);
     }
-    //connect contract 
-    const healthBlockContract = async () => {
-        try {
-            const web3modal = new Web3Modal();
-            const connection = await web3modal.connect();
-            const provider = new ethers.providers.Web3Provider(connection);
-            const signer = provider.getSigner();
-            const contract = await fetchContract(signer);
-        }
-        catch (err) {
-            setError(`Error Loading Health Contract ${err}`);
-        }
+  };
+  //connect contract
+  const registerHealthBlockContract = async (name: string, age: number, email: string) => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract: HealthBlock = fetchContract(signer);
+      const register = await contract.registerPatient(name, age, email);
+      register.wait();
+    } catch (err: any) {
+      setError(`Error Loading Health Contract ${err}`);
     }
-    //connect contract 
-    const registerHealthBlockContract = async (name: string, age: number, email: string) => {
-        try {
-            const web3modal = new Web3Modal();
-            const connection = await web3modal.connect();
-            const provider = new ethers.providers.Web3Provider(connection);
-            const signer = provider.getSigner();
-            const contract: HealthBlock = await fetchContract(signer);
-            const register = await contract.registerPatient(name, age, email);
-            register.wait();
-        }
-        catch (err:any) {
-            setError(`Error Loading Health Contract ${err}`);
-        }
+  };
+  const registerDoctorHealthBlockContract = async (
+    name: string,
+    age: number,
+    email: string,
+    specialization: string,
+  ) => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract: HealthBlock = fetchContract(signer);
+      const register = await contract.registerDoctor(name, age, email, specialization);
+      register.wait();
+    } catch (err) {
+      setError(`Error Loading Health Contract ${err}`);
     }
-    const registerDoctorHealthBlockContract = async (name: string, age: number, email: string, specialization: string) => {
-        try {
-            const web3modal = new Web3Modal();
-            const connection = await web3modal.connect();
-            const provider = new ethers.providers.Web3Provider(connection);
-            const signer = provider.getSigner();
-            const contract: HealthBlock = await fetchContract(signer);
-            const register = await contract.registerDoctor(name, age, email, specialization);
-            register.wait();
-        }
-        catch (err) {
-            setError(`Error Loading Health Contract ${err}`);
-        }
+  };
+  const fetchPatientContract = async () => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract: HealthBlock = fetchContract(signer);
+      const [name, age, email] = await contract.getPatientInfo();
+      return { name, age, email } as Patient;
+    } catch (err) {
+      setError(`Error Loading Health Contract ${err}`);
     }
-    const fetchPatientContract = async () => {
-        try {
-            const web3modal = new Web3Modal();
-            const connection = await web3modal.connect();
-            const provider = new ethers.providers.Web3Provider(connection);
-            const signer = provider.getSigner();
-            const contract: HealthBlock = await fetchContract(signer);
-            const info = await contract.getPatientInfo();
-        }
-        catch (err) {
-            setError(`Error Loading Health Contract ${err}`);
-        }
+  };
+  const fetchPatientInfoContract = async (address: string) => {
+    try {
+      const web3modal = new Web3Modal();
+      const connection = await web3modal.connect();
+      const provider = new ethers.providers.Web3Provider(connection);
+      const signer = provider.getSigner();
+      const contract: HealthBlock = fetchContract(signer);
+      const [name, age, email] = await contract.getPatientInfoAll(address);
+    } catch (err) {
+      setError(`Error Loading Health Contract ${err}`);
     }
-    const fetchPatientInfoContract = async (address: string) => {
-        try {
-            const web3modal = new Web3Modal();
-            const connection = await web3modal.connect();
-            const provider = new ethers.providers.Web3Provider(connection);
-            const signer = provider.getSigner();
-            const contract: HealthBlock = await fetchContract(signer);
-            const info = await contract.getPatientInfoAll(address);
-        }
-        catch (err) {
-            setError(`Error Loading Health Contract ${err}`);
-        }
-    }
-    return (<HealthContext.Provider value={{ checkIfWalletIsConnected, connectWallet, currentAccount, healthBlockContract, registerHealthBlockContract, fetchPatientContract, fetchPatientInfoContract, registerDoctorHealthBlockContract }}>{children}</HealthContext.Provider>)
-}
+  };
+  return (
+    <HealthContext.Provider
+      value={{
+        checkIfWalletIsConnected,
+        connectWallet,
+        currentAccount,
+        healthBlockContract,
+        registerHealthBlockContract,
+        fetchPatientContract,
+        fetchPatientInfoContract,
+        registerDoctorHealthBlockContract,
+      }}
+    >
+      {children}
+    </HealthContext.Provider>
+  );
+};
