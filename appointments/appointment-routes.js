8f0d93eae0857bc1f60dceeb20db7ba8706e18f5
Shruthi23 <shruthisrinivasan97@gmail.com>
2023-04-04 17:02:33 -0700

    HEAL-76-API integration to get all the appointments for agiven doctor and managing the appointments

--- appointments/appointment-routes.js
+++ appointments/appointment-routes.js
@@ -1,75 +1,80 @@
 const uuid = require("uuid");
 const AWS = require("aws-sdk");
+require("dotenv").config();
 
 AWS.config.update({
-    accessKeyId: 'AKIAWKISFSUDRPW5SMMP',
-    secretAccessKey: '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',
-    region: 'us-east-1'
-  });
-  const dynamodb =new AWS.DynamoDB.DocumentClient();
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 module.exports = (app) => {
   app.get("/getAppointments/:doctorEmail", (req, res) => {
-    // res.send(req.metaAuth.challenge);
-    //const appointmentId = uuid.v4();
     let { doctorEmail } = req.params;
-    console.log("dcotorEmail",doctorEmail)
-    const appointmentId = uuid.v4()
-    console.log("appointmentid--->",appointmentId)
 
     const params = {
       TableName: "appointments",
       KeyConditionExpression: "doctor_email = :doctorEmail",
       ExpressionAttributeValues: {
-        ":doctorEmail": "abc@gmail.com",
+        ":doctorEmail": doctorEmail,
       },
     };
 
     dynamodb.query(params, (err, data) => {
       if (err) {
         console.log("Error querying appointments: ", err);
-        res.send(err)
+        res.send(err);
       } else {
-        console.log("Appointments: ", data.Items);
-        res.status(200).send(data.Items?data.Items:data);
+        res.status(200).send(data.Items ? data.Items : data);
       }
     });
-   
   });
 
-app.put('/updateAppointmentStatus/:appointmentID/status',(req,res)=>{
-    const {appointmentId} = req.params
-    const {appointmentStatus} = req.body
-    console.log("appointmentid--->",req.params)
-    const db = new AWS.DynamoDB();
-   
-
+  app.put("/updateAppointmentStatus/:appointmentID/status", (req, res) => {
+    const { appointmentID } = req.params;
+    const { appointmentStatus } = req.body;
 
     const params = {
-        TableName: 'appointments',
-        Key:{
-            'doctor_email': { S: 'abc@gmail.com' },
-            //'appointment_id': { S: 'c8d947c1-f3cb-4510-bc94-e2c75b16b3e0'},
-            'created_at':{S:'2023-04-04'}
-        },
-        UpdateExpression: 'SET appointment_status = :newStatus',
-       
-        ExpressionAttributeValues: {
-          ':newStatus': {S:"reject"}
-        },
-      };
-      console.log("params",params)
-      
-      db.updateItem(params, function(err, data) {
-        if (err) {
-          console.log('Error', err);
-        } else {
-          console.log('Success', data);
-          res.send({"success":true})
-        }
-      });
+      TableName: "appointments",
+      FilterExpression: "appointment_id = :appointmentId",
+      ExpressionAttributeValues: {
+        ":appointmentId": appointmentID,
+      },
+    };
 
+    dynamodb.scan(params, (err, data) => {
+      if (err) {
+        console.error("Error scanning appointment", err);
+      } else {
+        console.log("Scan succeeded:", data);
+      }
+      if (data && data.Items && data.Items.length > 0) {
+        let results = data.Items[0];
+        const updateParams = {
+          TableName: "appointments",
+          Key: {
+            doctor_email: results.doctor_email,
+            created_at: results.created_at,
+          },
+          UpdateExpression: "set appointment_status = :status",
+          ExpressionAttributeValues: {
+            ":status": appointmentStatus,
+          },
+          ReturnValues: "ALL_NEW",
+        };
 
- })
+        dynamodb.update(updateParams, (err, data) => {
+          if (err) {
+            console.error("Error updating appointment status", err);
+            res.status(500).send('Error updating record');
 
+          } else {
+            console.log("Appointment status updated successfully", data);
+            res.status(200).json({ message: "Resource updated successfully" });
+          }
+        });
+      }
+    });
+  });
 };
