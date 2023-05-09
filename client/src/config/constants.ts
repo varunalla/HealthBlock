a0249d84443d8a4823374460d4caca810d28a482
Varun Alla <varun.alla@gmail.com>
2023-05-09 12:14:24 -0700

    Notifications toasts for logins (#23)
    * Notifications for logins
    
    * Added Try Catch

--- client/src/config/constants.ts
+++ client/src/config/constants.ts
@@ -2,4 +2,6 @@ import HealthBlock from '../contracts/HealthBlock.json';
 
 export const healthBlockAddress = process.env.REACT_APP_HEALTHBLOCK_ADDRESS || 'Test';
 export const providerBlockAddress = process.env.REACT_APP_PROVIDER_ADDRESS || 'Test';
+export const blockChainAddress =
+  process.env.REACT_APP_BLOCKCHAIN_ADDRESS || 'http://127.0.0.1:8545';
 export const healthBlockABI = HealthBlock.abi;
